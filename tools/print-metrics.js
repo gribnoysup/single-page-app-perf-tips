875ca31a3c369225f950d3de201a603a9af8bdeb
Sergey Petushkov <petushkov.sergey@gmail.com>
2018-03-15 18:56:46 +0100

    Tools: add color presets for charts

--- tools/print-metrics.js
+++ tools/print-metrics.js
@@ -7,6 +7,7 @@ const { width: terminalWidth } = require('window-size');
 
 const { getProjectDir } = require('./utils/paths');
 const logger = require('./utils/logger');
+const { blue } = require('./utils/colors');
 const f = require('./utils/format');
 
 const printMetrics = async (project, { metrics } = {}) => {
@@ -14,11 +15,9 @@ const printMetrics = async (project, { metrics } = {}) => {
   const metricsPath = path.join(projectDir, 'metrics.json');
 
   if (!Array.isArray(metrics) && !await fs.pathExists(metricsPath)) {
-    const message = `${f(
-      project
-    )} Couldn't find metrics.json for the application. Please run ${b(
-      'npm run measure'
-    )} first`;
+    const message =
+      `${f(project)} Couldn't find metrics.json for the application. ` +
+      `Please run ${b('npm run measure')} first`;
 
     logger.error(message);
     return;
@@ -41,14 +40,14 @@ const printMetrics = async (project, { metrics } = {}) => {
   const totalMax = Math.max(...allTimings);
 
   metrics.forEach(({ url, timings }) => {
-    const normalized = timings.map(({ timing, title }) => ({
+    const normalized = timings.map(({ timing, title }, index) => ({
       label: title,
       value: timing,
+      color: blue[index],
     }));
 
     const { chart, legend, scale } = wunderbar(normalized, {
       length: Math.min(terminalWidth, 128),
-      randomColorOptions: { seed: 'd', luminosity: 'bright' },
       min: totalMin,
       max: totalMax,
       format: '0',
